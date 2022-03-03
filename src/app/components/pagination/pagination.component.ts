18328cb9e420b468d6c805a78addaf304807107b
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2022-03-02 22:12:00 -0500

    ✨ update: add filter in pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -8,18 +8,19 @@ import { Book } from '../../interfaces';
   styleUrls: ['./pagination.component.css'],
 })
 export class PaginationComponent implements OnInit {
+  //
   data: Book[] = data;
   currentData: Book[] = [];
   page: number = 1;
-
   PAGESIZE: number = 10;
   pageList: number[] = [];
+  filterQueryString: string = 'The';
 
   getBookDetails(data: Book[], currentPage: number = 1) {
-    const pages = Math.ceil(this.data.length / this.PAGESIZE);
+    const pages = Math.ceil(data.length / this.PAGESIZE);
     this.pageList = Array.from({ length: pages }, (_, i) => i + 1);
     this.page = currentPage;
-    this.currentData = this.data.slice(
+    this.currentData = data.slice(
       (currentPage - 1) * this.PAGESIZE,
       currentPage * this.PAGESIZE
     );
@@ -35,5 +36,13 @@ export class PaginationComponent implements OnInit {
 
   ngOnInit() {
     this.getBookDetails(this.data);
+  }
+
+  onFilter(query: string) {
+    let newData = this.data.filter((book) => {
+      return book.title.toLowerCase().includes(query.toLowerCase());
+    });
+    this.page = 1;
+    this.getBookDetails(newData);
   }
 }
